f6f01879e6cf15900fe7f174368f7b3a24e18f72
GregQuiggWM <grquigg@email.wm.edu>
2020-05-01 22:55:34 -0400

    Changes to UI

--- Baseball/src/Baseball/SelectPlayersMenu.java
+++ Baseball/src/Baseball/SelectPlayersMenu.java
@@ -11,6 +11,8 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
@@ -42,19 +44,103 @@ public class SelectPlayersMenu extends JFrame implements ActionListener, ListSel
         
         JLabel setupTeam1 = new JLabel("Enter information for Team Black");
         gc.fill = GridBagConstraints.NONE;
-        gc.insets = new Insets(10, 10, 0, 0);
+        gc.insets = new Insets(10, 12, 0, 0);
         gc.anchor = gc.NORTHWEST;
-        gc.weightx = 1;
-        gc.weighty = 1;
+        //gc.weightx = 1;
+        gc.weighty = 0.01;
 		gc.gridx = 0;
 		gc.gridy = 0;
 		add(setupTeam1, gc);
 		
+		JLabel label1 = new JLabel("Name");
+		gc.weighty = 0.01;
+		gc.gridy = 1;
+		gc.insets = new Insets(0, 12, 0, 0);
+		add(label1, gc);
+		
+		gc.anchor = gc.NORTH;
+		gc.insets = new Insets(0, 10, 0, 0);
+		listPlayersTeam1 = new JList(listModelTeam1);
+        listPlayersTeam1.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        listPlayersTeam1.setSelectedIndex(0);
+        listPlayersTeam1.setVisibleRowCount(5);
+        JScrollPane listScrollPane = new JScrollPane(listPlayersTeam1);
+        gc.gridy = 2;
+        gc.weighty = 0.05;
+        gc.gridwidth = 2;
+        //gc.fill = GridBagConstraints.HORIZONTAL;
+        add(listScrollPane, gc);
+		
+        gc.fill = gc.NONE;
+        gc.anchor = gc.CENTER;
+        JButton button = new JButton("Select Player");
+        gc.insets = new Insets(0, 0, 0, 50);
+        gc.gridwidth = 1;
+        gc.gridy = 3;
+        gc.gridx = 1;
+        gc.weighty = 0.3;
+        //gc.ipadx = 60;
+        add(button, gc);
+        
+        JButton addNewTeamMember = new JButton("Add New Player");
+        gc.ipadx = 0;
+        gc.insets = new Insets(0, 20, 0, 0);
+        gc.gridx = 0;
+        add(addNewTeamMember, gc);
+        
+        gc.insets = new Insets(10, 12, 0, 0);
+        gc.anchor = gc.NORTHWEST;
 		JLabel setupTeam2 = new JLabel("Enter information for Team White");
 		gc.gridx = 0;
-		gc.gridy = 1;
+		gc.gridy = 4;
+		gc.weighty = 0.01;
 		add(setupTeam2, gc);
-		setSize(500, 800);
+		
+		JLabel label2 = new JLabel("Name");
+		gc.weighty = 0.01;
+		gc.gridy = 5;
+		gc.insets = new Insets(0, 12, 0, 0);
+		add(label2, gc);
+		
+		gc.anchor = gc.NORTH;
+		gc.gridwidth = 2;
+		listPlayersTeam2 = new JList(listModelTeam1);
+        listPlayersTeam2.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        listPlayersTeam2.setSelectedIndex(0);
+        listPlayersTeam2.setVisibleRowCount(5);
+        JScrollPane listScrollPane2 = new JScrollPane(listPlayersTeam2);
+        gc.gridy = 6;
+        //gc.gridx = 0;
+        gc.weighty = 0.03;
+        gc.ipadx = 100;
+        gc.gridwidth = 2;
+        //gc.fill = GridBagConstraints.HORIZONTAL;
+        add(listScrollPane2, gc);
+        
+        gc.fill = gc.NONE;
+        gc.anchor = gc.CENTER;
+        gc.ipadx = 0;
+        JButton button2 = new JButton("Select Player");
+        gc.insets = new Insets(0, 0, 0, 50);
+        gc.gridwidth = 1;
+        gc.gridy = 7;
+        gc.gridx = 1;
+        gc.weighty = 0.3;
+        add(button2, gc);
+        
+        JButton addNewTeamMember2 = new JButton("Add New Player");
+        gc.insets = new Insets(0, 20, 0, 0);
+        gc.gridx = 0;
+        add(addNewTeamMember2, gc);
+        
+        JLabel lab = new JLabel("Section for lineups");
+        gc.insets = new Insets(10, 12, 0, 0);
+        gc.anchor = gc.CENTER;
+        gc.gridx = 2;
+        gc.gridy = 0;
+        gc.weightx = 1;
+        add(lab, gc);
+		setSize(800, 500);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 
